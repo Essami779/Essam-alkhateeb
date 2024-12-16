e5d2b80dd3a1286c807e79134bc78e97810911b4
Essam Alkhateeb <essamalkhateeb88@gmail.com>
2024-12-16 17:57:00 +0300

    change lang & translate

--- js-ar/script.js
+++ js-ar/script.js
@@ -1,107 +1,121 @@
 /* =================================== typing animation  ================================= */
-var typed = new Typed(".typing", {
-    strings: ["مصمم ويب", "مطور ويب", "مستقل"],
+var typed = new Typed(".typing",{
+    strings : ["مصمم مواقع","مطور مواقع","عمل حر"],
     typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
-
+    BaskSpeed:60,
+    loop:true
+})
 /* =================================== Aside  ================================= */
-const nav = document.querySelector(".nav"),
+const nav =document.querySelector(".nav"),
     navList = nav.querySelectorAll("li"),
-    totalNavList = navList.length,
+    totalNavList =navList.length,
 
+    
     allSection = document.querySelectorAll(".section"),
     totalSection = allSection.length;
-
-for (let i = 0; i < totalNavList; i++) {
-    const a = navList[i].querySelector("a");
-    a.addEventListener("click", function () {
-        removeBackSection();
-        for (let j = 0; j < totalNavList; j++) {
-            if (navList[j].querySelector("a").classList.contains("active")) {
-                addBackSection(j);
+    for(let i=0; i<totalNavList; i++)
+    {
+        const a= navList[i].querySelector("a")
+        a.addEventListener("click" , function()
+        {
+            removeBackSection();
+              for(let j=0; j<totalNavList; j++)
+            {
+                if(navList[j].querySelector("a").classList.contains("active"))
+                {
+                    addBackSection(j);
+                   // allSection[j].classList.add("back-section");
+                }
+                navList[j].querySelector("a").classList.remove("active");
             }
-            navList[j].querySelector("a").classList.remove("active");
+            this.classList.add("active")
+            showSection(this);
+            if(window.innerWidth <1200)
+            {
+                asideSectionTogglerBtn();
+            }
+        })
+
+    }
+    function removeBackSection()
+    {
+        for(let i=0; i<totalSection; i++)
+        {
+            allSection[i].classList.remove(".back-section");
         }
-        this.classList.add("active");
+    }
+    function addBackSection(num)
+    {
+        allSection[num].classList.add("back-section");
+    }
+    function showSection(element)
+    {
+        for(let i=0; i<totalSection; i++)
+        {
+            allSection[i].classList.remove("active");
+        }
+       const target = element.getAttribute("href").split("#")[1];
+       document.querySelector("#" + target).classList.add("active")
+    }
+    function updateNav(element)
+    {
+        for(let i=0; i<totalNavList ; i++)
+        {
+            navList[i].querySelector("a").classList.remove("active");
+            const target = element.getAttribute("href").split("#")[1];
+            if(target === navList[i].querySelector("a").getAttribute("href").split("#")[i])
+            {
+                navList[i].querySelector("a").classList.add("active");
+            }
+        }
+    }
+    document.querySelector(".hire-me").addEventListener("click",function()
+    {
+        const sectionIndex = this.getAttribute("data-section-index");
+       // console.log(sectionIndex);
         showSection(this);
-        if (window.innerWidth < 1200) {
-            asideSectionTogglerBtn();
-        }
-    });
-}
-
-function removeBackSection() {
-    for (let i = 0; i < totalSection; i++) {
-        allSection[i].classList.remove("back-section");
-    }
-}
-
-function addBackSection(num) {
-    allSection[num].classList.add("back-section");
-}
-
-function showSection(element) {
-    for (let i = 0; i < totalSection; i++) {
-        allSection[i].classList.remove("active");
-    }
-    const target = element.getAttribute("href").split("#")[1];
-    document.querySelector("#" + target).classList.add("active");
-}
-
-function updateNav(element) {
-    for (let i = 0; i < totalNavList; i++) {
-        navList[i].querySelector("a").classList.remove("active");
-        const target = element.getAttribute("href").split("#")[1];
-        if (target === navList[i].querySelector("a").getAttribute("href").split("#")[1]) {
-            navList[i].querySelector("a").classList.add("active");
-        }
-    }
-}
-
-document.querySelector(".hire-me").addEventListener("click", function () {
-    const sectionIndex = this.getAttribute("data-section-index");
-    showSection(this);
-    updateNav(this);
-    removeBackSection();
-    addBackSection(sectionIndex);
-});
-
-const navTogglerBtn = document.querySelector(".nav-toggler"),
+        updateNav(this);
+        removeBackSection();
+        addBackSection(sectionIndex);
+    })
+    const navTogglerBtn = document.querySelector(".nav-toggler"),
     aside = document.querySelector(".aside");
-navTogglerBtn.addEventListener("click", () => {
-    asideSectionTogglerBtn();
-});
-
-function asideSectionTogglerBtn() {
-    aside.classList.toggle("open");
-    navTogglerBtn.classList.toggle("open");
-    for (let i = 0; i < totalSection; i++) {
-        allSection[i].classList.toggle("open");
+    navTogglerBtn.addEventListener( "click", () =>
+    {
+        asideSectionTogglerBtn();
+    })
+    function asideSectionTogglerBtn()
+    {
+        aside.classList.toggle("open");
+        navTogglerBtn.classList.toggle("open");
+        for(let i=0; i<totalSection; i++)
+        {
+            allSection[i].classList.toggle("open");
+        }
     }
-}
 
-// email.js
-function sendMail() {
-    var params = {
+
+    //email.js
+        function sendMail() {
+        var params = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
-        subject: document.getElementById("subject").value, // تصحيح خطأ إملائي
+        seubject:document.getElementById|("seubject").value,
         message: document.getElementById("message").value,
-    };
-
-    const serviceID = "service_3vtz3ss";
-    const templateID = "template_awje3wb";
-
-    emailjs.send(serviceID, templateID, params)
-        .then(res => {
-            document.getElementById("name").value = "";
-            document.getElementById("email").value = "";
-            document.getElementById("subject").value = ""; // تصحيح خطأ إملائي
-            document.getElementById("message").value = "";
-            console.log(res);
-            alert("تم إرسال رسالتك بنجاح!!");
-        })
-        .catch(err => console.log(err));
-}
+        };
+      
+        const serviceID = "service_3vtz3ss";
+        const templateID = "template_awje3wb";
+      
+          emailjs.send(serviceID, templateID, params)
+          .then(res=>{
+              document.getElementById("name").value = "";
+              document.getElementById("email").value = "";
+              document.getElementById("seubject").value = "";
+              document.getElementById("message").value = "";
+              console.log(res);
+              alert("Your message sent successfully!!")
+      
+          })
+          .catch(err=>console.log(err));
+      }
